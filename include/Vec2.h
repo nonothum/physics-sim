fbbc0d70d668af71a1c9fae22b7850c2adfceffb
Jasper Chong <skoob@doob.com>
2026-05-18 13:01:54 -0400

    Add Vec2 operators

--- include/Vec2.h
+++ include/Vec2.h
@@ -7,6 +7,15 @@ struct Vec2 {
     Vec2() = default;
     Vec2(float x, float y) : x(x), y(y) {}
 
+    Vec2 operator+(const Vec2& o) const { return {x + o.x, y + o.y}; }
+    Vec2 operator-(const Vec2& o) const { return {x - o.x, y - o.y}; }
+    Vec2 operator*(float s)        const { return {x * s,   y * s};   }
+    Vec2 operator/(float s)        const { return {x / s,   y / s};   }
+
+    Vec2& operator+=(const Vec2& o) { x += o.x; y += o.y; return *this; }
+    Vec2& operator-=(const Vec2& o) { x -= o.x; y -= o.y; return *this; }
+    Vec2& operator*=(float s)       { x *= s;   y *= s;   return *this; }
+
     float length() const { return std::sqrt(x*x + y*y); }
     float lengthSq() const { return x*x + y*y; }
     float dot(const Vec2& o) const { return x*o.x + y*o.y; }
